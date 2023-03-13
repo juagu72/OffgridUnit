92f5ff70f60c7da5a310a8e960098fd887447043
Juri Guldner <J.Guldner@automationwr.de>
2023-03-13 19:53:54 +0100

    OTA

--- ESP32 Firmware/VEDirectTester/VEDirectTester.ino
+++ ESP32 Firmware/VEDirectTester/VEDirectTester.ino
@@ -5,6 +5,9 @@
 #include <HardwareSerial.h>
 #include "VeDirectFrameHandler.h"
 #include "time.h"
+#include "WebServer.h" // OTA
+#include "ESPmDNS.h"
+#include "Update.h"
 
 // VE.Direct
 //
@@ -13,7 +16,6 @@
 VeDirectFrameHandler myve;
 //SoftwareSerial veSerial(rxPin, txPin);
 HardwareSerial veSerial(1);
- 
 
 // WiFi AP
 //
@@ -45,6 +47,95 @@ PubSubClient mqttClient(wifiClient);
 InfluxDBClient influxClient(INFLUXDB_URL, INFLUXDB_ORG, INFLUXDB_BUCKET, INFLUXDB_TOKEN, InfluxDbCloud2CACert);
 Point influxDataPoint("VEDirectDataPoint");
 
+// OTA
+WebServer webServer(80);
+const char* host = "esp32";
+/*
+ * Login page
+ */
+const char* loginIndex =
+ "<form name='loginForm'>"
+    "<table width='20%' bgcolor='A09F9F' align='center'>"
+        "<tr>"
+            "<td colspan=2>"
+                "<center><font size=4><b>ESP32 Login Page</b></font></center>"
+                "<br>"
+            "</td>"
+            "<br>"
+            "<br>"
+        "</tr>"
+        "<tr>"
+             "<td>Username:</td>"
+             "<td><input type='text' size=25 name='userid'><br></td>"
+        "</tr>"
+        "<br>"
+        "<br>"
+        "<tr>"
+            "<td>Password:</td>"
+            "<td><input type='Password' size=25 name='pwd'><br></td>"
+            "<br>"
+            "<br>"
+        "</tr>"
+        "<tr>"
+            "<td><input type='submit' onclick='check(this.form)' value='Login'></td>"
+        "</tr>"
+    "</table>"
+"</form>"
+"<script>"
+    "function check(form)"
+    "{"
+    "if(form.userid.value=='admin' && form.pwd.value=='admin')"
+    "{"
+    "window.open('/serverIndex')"
+    "}"
+    "else"
+    "{"
+    " alert('Error Password or Username')/*displays error message*/"
+    "}"
+    "}"
+"</script>";
+
+/*
+ * Server Index Page
+ */
+const char* serverIndex =
+"<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>"
+"<form method='POST' action='#' enctype='multipart/form-data' id='upload_form'>"
+   "<input type='file' name='update'>"
+        "<input type='submit' value='Update'>"
+    "</form>"
+ "<div id='prg'>progress: 0%</div>"
+ "<script>"
+  "$('form').submit(function(e){"
+  "e.preventDefault();"
+  "var form = $('#upload_form')[0];"
+  "var data = new FormData(form);"
+  " $.ajax({"
+  "url: '/update',"
+  "type: 'POST',"
+  "data: data,"
+  "contentType: false,"
+  "processData:false,"
+  "xhr: function() {"
+  "var xhr = new window.XMLHttpRequest();"
+  "xhr.upload.addEventListener('progress', function(evt) {"
+  "if (evt.lengthComputable) {"
+  "var per = evt.loaded / evt.total;"
+  "$('#prg').html('progress: ' + Math.round(per*100) + '%');"
+  "}"
+  "}, false);"
+  "return xhr;"
+  "},"
+  "success:function(d, s) {"
+  "console.log('success!')"
+ "},"
+ "error: function (a, b, c) {"
+ "}"
+ "});"
+ "});"
+ "</script>";
+
+
 // Test
 //
 int counter = 0;
@@ -66,6 +157,8 @@ void setup() {
     delay(500);
   }
   Serial.println();
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
 
   mqttClient.setServer(mqttServerIP, 1883);
 
@@ -75,16 +168,69 @@ void setup() {
   if (influxClient.validateConnection()) {
     Serial.print("Connected to InfluxDB: ");
     Serial.println(influxClient.getServerUrl());
+    influxDataPoint.clearFields();
+    influxDataPoint.setTime();
+    influxDataPoint.addField("Log", "Setup");
+    if (!influxClient.writePoint(influxDataPoint)) {
+      Serial.print("InfluxDB write failed: ");
+      Serial.println(influxClient.getLastErrorMessage());
+    }
   } else {
     Serial.print("InfluxDB connection failed: ");
     Serial.println(influxClient.getLastErrorMessage());
   }
 
+  /*use mdns for host name resolution*/
+  if (!MDNS.begin(host)) { //http://esp32.local
+    Serial.println("Error setting up MDNS responder!");
+    while (1) {
+      delay(1000);
+    }
+  }
+  Serial.println("mDNS responder started");
+  /*return index page which is stored in serverIndex */
+  webServer.on("/", HTTP_GET, []() {
+    webServer.sendHeader("Connection", "close");
+    webServer.send(200, "text/html", loginIndex);
+  });
+  webServer.on("/serverIndex", HTTP_GET, []() {
+    webServer.sendHeader("Connection", "close");
+    webServer.send(200, "text/html", serverIndex);
+  });
+  /*handling uploading firmware file */
+  webServer.on("/update", HTTP_POST, []() {
+    webServer.sendHeader("Connection", "close");
+    webServer.send(200, "text/plain", (Update.hasError()) ? "FAIL" : "OK");
+    ESP.restart();
+  }, []() {
+    HTTPUpload& upload = webServer.upload();
+    if (upload.status == UPLOAD_FILE_START) {
+      Serial.printf("Update: %s\n", upload.filename.c_str());
+      if (!Update.begin(UPDATE_SIZE_UNKNOWN)) { //start with max available size
+        Update.printError(Serial);
+      }
+    } else if (upload.status == UPLOAD_FILE_WRITE) {
+      /* flashing firmware to ESP*/
+      if (Update.write(upload.buf, upload.currentSize) != upload.currentSize) {
+        Update.printError(Serial);
+      }
+    } else if (upload.status == UPLOAD_FILE_END) {
+      if (Update.end(true)) { //true to set the size to the current progress
+        Serial.printf("Update Success: %u\nRebooting...\n", upload.totalSize);
+      } else {
+        Update.printError(Serial);
+      }
+    }
+  });
+
+  webServer.begin();
 }
 
 void loop() {
   if (wifiMulti.run() == WL_CONNECTED){
-    
+    webServer.handleClient();
+    delay(1);
+  
     if (!mqttClient.connected()){
       reconnect();
     }
@@ -214,7 +360,7 @@ void PublishData2DB() {
     }
   }
 
-  if (!influxClient.writePoint(influxDataPoint)) {
+  if ((myve.veEnd > 0) && (!influxClient.writePoint(influxDataPoint))) {
     Serial.print("InfluxDB write failed: ");
     Serial.println(influxClient.getLastErrorMessage());
   }
